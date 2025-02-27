aadc6b10dffb81fd15a916735e4d7e3a11439be9
Sandra Castro España <sandracaes00@gmail.com>
2025-02-26 22:45:00 -0600

    Se agrega archivo html y js correspondientes al proyecto de la segunda lección sobre comparaciones y decisiones del módulo 03 del Master en Front-End

---
+++ clase-2/evaluador-notas.js
@@ -0,0 +1,17 @@
+let nota = Math.floor(Math.random() * 101);
+
+if (nota >= 0) {
+console.log(`La calificación del estudiante es: ${nota}`);
+
+if (nota >= 90) {
+  console.log("Excelente");
+} else if (nota >= 75 && nota <= 89) {
+  console.log("Bien");
+} else if (nota >= 60 && nota <= 74) {
+  console.log("Suficiente");
+} else {
+  console.log("Estudiante no aprueba");
+}
+} else {
+  console.log("Calificación no valida");
+}
